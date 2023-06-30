e59890f30ae3259c1d4d87a27f0fa3293515a9fa
Maritariny <marina0391@mail.ru>
2023-06-30 22:10:41 +0300

    fast (by w)

--- src/ru/maritariny/Main.java
+++ src/ru/maritariny/Main.java
@@ -9,169 +9,97 @@ public class Main {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         //BufferedReader reader = new BufferedReader(new FileReader("test.txt"));
-
         String s = reader.readLine();
         int n = Integer.parseInt(reader.readLine());
-        List<Long> list = RLE(s);
-        Map<Point, Long> map = new HashMap<>(n);
-        List<Point> listPoint = new ArrayList<>(n);
-        int length = list.size();
-        for (int i = 0; i < n; i++) {
+        List<long[]> list = new ArrayList<>();
+        char[] ss = s.toCharArray();
+        int i = 0;
+        long sum = 0; // нарастающим итогом длина исходной строки
+        long sumRle = 0; // нарастающим итогом длина сжатой строки
+        while (i < ss.length) {
+            long beginI = i;
+            char ch = ss[i];
+            while (!Character.isLetter(ch)) {
+                i++;
+                ch = ss[i];
+            }
+            long newSumRle = i - beginI + 1;
+            long newSum = 0;
+            if (i - beginI <= 0) {
+                newSum = 1;
+            } else {
+                newSum = Long.parseLong(s.substring((int)beginI, i));
+            }
+            sum += newSum;
+            sumRle += newSumRle;
+            list.add(new long[]{sum, sumRle});
+            i++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < n; j++) {
             String[] parts = reader.readLine().split(" ");
             long left = Long.parseLong(parts[0]);
             long right = Long.parseLong(parts[1]);
-            listPoint.add(new Point(left, right));
-        }
-        List<Point> sortedList = new ArrayList<>(listPoint);
-        Collections.sort(sortedList);
-        int first = 0;
-        for (Point point : sortedList) {
-            long left = point.getLeft();
-            long right = point.getRight();
-            int begin = first; // номер начала интервала в списке list. Начала интервалов упорядочены
-            long cur = list.get(first);
-            if (cur != left) {
-                for (int i = (first + 1); i < length; i++) {
-                    if (list.get(i) == left) {
-                        begin = i;
-                        first = i;
-                        break;
-                    } else if (list.get(i) > left) {
-                        begin = i - 1;
-                        first = i - 1;
-                        break;
-                    }
+            if (right - left <= 1) {
+                sb.append(right - left + 1);
+                sb.append("\n");
+                continue;
+            }
+            int indexLeft = binarySearch(list, left);
+            int indexRight = binarySearch(list, right);
+            long res = 0;
+            if (indexLeft == indexRight) {
+                if ((right - left) <= 1) {
+                    res = right - left + 1;
+                } else {
+                    long d = right - left + 1;
+                    res = (long)Math.ceil(Math.log10(d + 0.1)) + 1;
+                }
+            } else {
+                long d = list.get(indexLeft)[0] - left + 1;
+                if (d == 1) {
+                    res = 1;
+                } else {
+                    res = (long)Math.ceil(Math.log10(d + 0.1)) + 1;
+                }
+                d = right - list.get(indexRight - 1)[0];
+                if (d == 1) {
+                    res++;
+                } else {
+                    res += (long)Math.ceil(Math.log10(d + 0.1)) + 1;
+                }
+                if (indexRight - indexLeft > 1) {
+                    res += list.get(indexRight - 1)[1] - list.get(indexLeft)[1];
                 }
             }
-            long result = 0L;
-            long next;
-            boolean end = false;
-            while (left <= right && !end) {
-                if ((begin + 1) <= (list.size() - 1)) {
-                    if (right < list.get(begin + 1)) {
-                       next = right + 1;
-                       end = true;
-                    } else {
-                       next =  list.get(begin + 1);
-                    }
-                } else {
-                    next = right + 1;
-                }
-                long d = next - left;
-                if (d == 0) {
-                    result++;
-                } else if (d <= 2) {
-                    result += d;
-                } else {
-                    result = result + (long)Math.ceil(Math.log10(d + 0.1)) + 1;
-                }
-                left = next;
-                begin++;
-            }
-            map.put(point, result);
-            //System.out.println(result);
+            sb.append(res);
+            sb.append("\n");
         }
 
-        for (Point point : listPoint) {
-            System.out.println(map.get(point));
-        }
+        System.out.println(sb);
         reader.close();
     }
-
-    private static List<Long> RLE(String s) {
-        List<Long> list = new ArrayList<>();
-        char[] ss = s.toCharArray();
-        boolean lastIsLetter = false;
-        StringBuilder curNumber = new StringBuilder();
-        Long it = 1L;
-        if (ss.length == 1) {
-            list.add(it);
-            return list;
-        } else if (ss.length == 2 && Character.isLetter(ss[0])) {
-            list.add(1L);
-            list.add(2L);
-            return list;
+    private static int binarySearch(List<long[]> list, long num) {
+        if (num <= list.get(0)[0]) {
+            return 0;
         }
-        boolean firstIter = true;
-        boolean lastLetterIsOK = true;
-        for (char ch : ss) {
-            boolean isLetter = Character.isLetter(ch);
-            if (isLetter) {
-                if (firstIter) {
-                    firstIter = false;
-                    lastIsLetter = isLetter;
-                    continue;
-                }
-                if (curNumber.length() != 0) {
-                    long number = Long.parseLong(curNumber.toString());
-                    list.add(it); // начало цифр
-                    it = it + number;
-                    lastLetterIsOK = false;
-                } else {
-                    if (lastIsLetter && (lastLetterIsOK)) {
-                        list.add(it); // буква сразу после цифр
-                        it++;
-                    } else {
-                        lastLetterIsOK = true;
-                    }
-                }
-                curNumber.setLength(0);
+        int length = list.size();
+        if (num == list.get(length - 1)[0]) {
+            return length - 1;
+        }
+        int begin = 0;
+        int end = length - 1;
+        while (begin + 1 < end) {
+            int mid = (begin + end) / 2;
+            if (list.get(mid)[0] == num) {
+                return mid;
+            } else if (list.get(mid)[0] >num) {
+                end = mid;
             } else {
-                firstIter = false;
-                curNumber.append(ch);
-                if (lastIsLetter && lastLetterIsOK) {
-                    list.add(it); // символ перед цифрой
-                    it++;
-                }
+                begin = mid;
             }
-            lastIsLetter = isLetter;
         }
-        if (lastIsLetter && lastLetterIsOK) {
-            list.add(it); // последний символ буква, которая не после цифры
-        }
-        //list.add(it - 1);
-
-        return list;
-    }
-
-}
-class Point implements Comparable<Point>{
-    private long left;
-    private long right;
-
-    public Point(long left, long right) {
-        this.left = left;
-        this.right = right;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return left == point.left && right == point.right;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(left, right);
-    }
-
-    @Override
-    public int compareTo(Point o) {
-        if (this.left == o.getLeft()) {
-            return Long.compare(this.right, o.getRight());
-            //return o.getLeft() - this.left;
-        }
-        return Long.compare(this.left, o.getLeft());
-        //return o.getW() - this.right;
-    }
-
-    public long getLeft() {
-        return left;
-    }
-
-    public long getRight() {
-        return right;
+        return begin + 1;
     }
 }
